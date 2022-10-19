926b7950b32c7141396835ba384dfbd83e42c045
altheradmin <91630778+altheradmin@users.noreply.github.com>
2022-10-19 08:26:14 -0300

    Update Underflix.jsx
    cmtchga

--- src/pages/Underflix.jsx
+++ src/pages/Underflix.jsx
@@ -73,15 +73,7 @@ console.log(movies);
 
      <Footer />
         
-      
-        {
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  // true for mobile device
-  alert("mobile device");
-}else{
-  // false for not mobile device
-  alert("not mobile device");
-}
+
       }
 
         
