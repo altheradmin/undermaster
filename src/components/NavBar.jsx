1feb150845ea2fbd6cc8fc8895dcf109436c09f5
altheradmin <91630778+altheradmin@users.noreply.github.com>
2025-04-19 12:21:43 -0300

    Update NavBar.jsx
    poisonheart

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -85,11 +85,11 @@ export default function NavBar({ isScrolled, currentEmail }) {
 <h1>{'Pedido de Filme/Série'}</h1>
 <h6>{'Responderei pelo email cadastrado:'} <div className='blurr'>{currentEmail}</div></h6>
 <div className="texto">
-<h7 >{'Informe o NOME e ANO do FILME ou SÉRIE desejada no espaço abaixo, sinta se a vontade para comentar sobre o filme e passar outro contato pela mensagem. *Respondo quando possível :)'}</h7>
+<h7 >{'Me manda nas redes sociais, não to acessando o admin daqui :) '}</h7>
 </div>
 </div>
 <div className="infoM">
- <textarea onChange={(e) => setPedido(e.target.value)} className="text"> </textarea>
+
 </div>
 <div className="flex">
 
@@ -119,7 +119,7 @@ export default function NavBar({ isScrolled, currentEmail }) {
 <h1>{'Ajuda'}</h1>
 <h6>{'Responderei pelo email cadastrado:'} <div className='blurr'>{currentEmail}</div></h6>
 <div className="texto">
-<h7 >{'Estou fazendo a documentação desse site, para mais informações entrar em contato, ou mandar mensagem diretamente abaixo *Respondo quando possível :)'}</h7>
+<h7 >{''Me manda nas redes sociais, não to acessando o admin daqui :) ''}</h7>
 </div>
 </div>
 <div className="infoM">
